c86ae16fb7df058d10f2e1cf3934356ec22b9edb
pierre lepers <pierre.lepers@gmail.com>
2019-11-28 16:34:27 +0100

    upgrade ci node version - upgrade SL browser list

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration
 // Generated on Sat Dec 19 2015 12:50:43 GMT+0100 (CET)
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test/*.js',
-      {pattern: 'test/assets/*.*', watched: false, included: false, served: true, nocache: false}
+      { pattern: 'test/assets/*.*', watched: false, included: false, served: true, nocache: false }
     ],
 
     proxies: {
@@ -32,13 +32,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.js': [ 'browserify' ]
+      'test/*.js': ['browserify']
     },
 
     browserify: {
       debug: true,
       transform: [
-        ['stringify', {'extensions': ['.vert', '.frag']}]
+        ['stringify', { 'extensions': ['.vert', '.frag'] }]
       ]
     },
 
@@ -88,81 +88,51 @@ module.exports = function(config) {
     // define SL browsers
     customLaunchers: {
 
-      // OSX Maverick
 
-      // 'SL_Chrome44_OSX10_10': {
-      //   base: 'SauceLabs',
-      //   browserName: 'chrome',
-      //   version: '44',
-      //   platform: 'OS X 10.10'
-      // },
-      // 'SL_Firefox38_OSX10_10': {
-      //   base: 'SauceLabs',
-      //   browserName: 'firefox',
-      //   version: '38',
-      //   platform: 'OS X 10.10'
-      // },
-      // 'SL_Firefox42_OSX10_10': {
-      //   base: 'SauceLabs',
-      //   browserName: 'firefox',
-      //   version: '42',
-      //   platform: 'OS X 10.10'
-      // },
-      // 'SL_Safari': {
-      //   base: 'SauceLabs',
-      //   browserName: 'safari',
-      //   platform: 'OS X 10.10',
-      //   version: '8'
-      // },
+      'SL_Chrome78_OSX10_13': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        version: '78.0',
+        platform: 'macOS 10.13',
+        webgl2: true
+      },
+      'SL_Chrome78_WIN10': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        version: '78.0',
+        platform: 'Windows 10',
+        webgl2: true
+      },
 
-      // //Win 8.1
-      // 'SL_Chrome44_WIN81': {
-      //   base: 'SauceLabs',
-      //   browserName: 'chrome',
-      //   version: '44',
-      //   platform: 'Windows 8.1'
-      // },
 
-      // 'SL_Firefox_WIN81': {
-      //   base: 'SauceLabs',
-      //   browserName: 'firefox',
-      //   version: '27',
-      //   platform: 'Windows 8.1'
-      // },
+      'SL_Chrome65_WIN10': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        version: '65.0',
+        platform: 'Windows 10',
+        webgl2: true
+      },
 
-      //Linux
-      // 'SL_Chrome_NIX': {
-      //   base: 'SauceLabs',
-      //   browserName: 'chrome',
-      //   version: '32',
-      //   platform: 'Linux'
-      // },
-      // 'SL_Firefox_NIX': {
-      //   base: 'SauceLabs',
-      //   browserName: 'firefox',
-      //   version: '27',
-      //   platform: 'Linux'
-      // },
-      // 'SL_Chrome_OSX9': {
-      //   base: 'SauceLabs',
-      //   browserName: 'chrome',
-      //   version: '31',
-      //   platform: 'OS X 10.9'
-      // },
       'SL_win10_Edge': {
         base: 'SauceLabs',
         browserName: 'MicrosoftEdge',
         platform: 'Windows 10',
-        version: '20.10240'
+        version: '18.17763'
+      },
+      'SL_win10_FF': {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 10',
+        version: '70.0'
       },
     },
   });
 
-  if( process.env.TRAVIS ) {
+  if (process.env.TRAVIS) {
 
     var browsers = [];
-    for( var browser in config.customLaunchers ){
-      browsers.push( browser );
+    for (var browser in config.customLaunchers) {
+      browsers.push(browser);
     }
     config.browsers = browsers;
 
